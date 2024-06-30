ace352975f93baff0f488bce00596a79221d11fc
RafaelZingra <rafaelzingra@hotmail.com>
2024-06-30 13:13:31 -0300

    creates expenses object on submit

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+const form = document.querySelector("form")
+
 const onlyNumbersRegex = /\D+/g
 
 amount.oninput = () => {
     let value = amount.value.replace(onlyNumbersRegex, "")
     value = Number(value)/100
-    amount.value = formatCurrencyBRL(value)    
+    amount.value = formatCurrencyBRL(value)
 }
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+    
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        caregoryId: category.value,
+        categoryName: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        createdAr: new Date()
+    }
+
+
+        
+    
+}
+
 
 function formatCurrencyBRL(value) {
     
     value = value.toLocaleString('pt-BR',{
         style: 'currency',
         currency: 'BRL'
-    })    
+    })
+
     return value
 }
